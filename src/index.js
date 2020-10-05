59322f96f631d54cd4868838398353a3a0113212
Josafat <josamtz@outlook.com>
2020-10-05 04:32:52 -0500

    🔥👨🏻‍💻 Font Awesome

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// Components
+import LandingPage from "./component/landingPage/LandingPage";
+// ServiceWorker
+import * as serviceWorker from "./serviceWorker";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "aos/dist/aos.css";
-import React from "react";
-import ReactDOM from "react-dom";
 import "./sass/style.sass";
 import "./shared/i18n";
-import LandingPage from "./component/landingPage/LandingPage";
-import * as serviceWorker from "./serviceWorker";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faLongArrowAltRight,
+  faLongArrowAltLeft,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faLongArrowAltRight, faLongArrowAltLeft);
 
 ReactDOM.render(<LandingPage />, document.getElementById("root"));
 
